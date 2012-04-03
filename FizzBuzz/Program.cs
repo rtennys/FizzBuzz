38342e80385577f35ef2bad69a7dc524286385c4
Rob Tennyson <rob@robtennyson.us>
2012-04-03 15:31:13 -0500

    First commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var fizz = "soda";
+            var buzz = "pop";
+            var fizzBuzz = "cola";
+
+            var fizzMultiple = 2;
+            var buzzMultiple = 7;
+
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i % fizzMultiple == 0 && i % buzzMultiple == 0) Console.WriteLine(fizzBuzz);
+                else if (i % fizzMultiple == 0) Console.WriteLine(fizz);
+                else if (i % buzzMultiple == 0) Console.WriteLine(buzz);
+                else Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+FizzBuzz
+=============
+
+Classic simple programming problem to filter out the programmers who can't write a simple program
+
+See [here](http://www.codinghorror.com/blog/2007/02/why-cant-programmers-program.html) for more
